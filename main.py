566664159121e8d65fd0f4966a557d20ec9a41c6
Abien Fred Agarap <abien_agarap@dlsu.edu.ph>
2019-12-06 21:55:46 +0800

    refactor: reformat files

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-__version__ = '0.1.0'
-__author__ = 'Abien Fred Agarap'
+__version__ = "0.1.0"
+__author__ = "Abien Fred Agarap"
 
 import argparse
 from model.cnn_softmax import CNN
@@ -27,23 +27,42 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='CNN & CNN-SVM for Image Classification')
-    group = parser.add_argument_group('Arguments')
-    group.add_argument('-m', '--model', required=True, type=str,
-                       help='[1] CNN-Softmax, [2] CNN-SVM')
-    group.add_argument('-d', '--dataset', required=True, type=str,
-                       help='path of the MNIST dataset')
-    group.add_argument('-p', '--penalty_parameter', required=False, type=int,
-                       help='the SVM C penalty parameter')
-    group.add_argument('-c', '--checkpoint_path', required=True, type=str,
-                       help='path where to save the trained model')
-    group.add_argument('-l', '--log_path', required=True, type=str,
-                       help='path where to save the TensorBoard logs')
+    parser = argparse.ArgumentParser(
+        description="CNN & CNN-SVM for Image Classification"
+    )
+    group = parser.add_argument_group("Arguments")
+    group.add_argument(
+        "-m", "--model", required=True, type=str, help="[1] CNN-Softmax, [2] CNN-SVM"
+    )
+    group.add_argument(
+        "-d", "--dataset", required=True, type=str, help="path of the MNIST dataset"
+    )
+    group.add_argument(
+        "-p",
+        "--penalty_parameter",
+        required=False,
+        type=int,
+        help="the SVM C penalty parameter",
+    )
+    group.add_argument(
+        "-c",
+        "--checkpoint_path",
+        required=True,
+        type=str,
+        help="path where to save the trained model",
+    )
+    group.add_argument(
+        "-l",
+        "--log_path",
+        required=True,
+        type=str,
+        help="path where to save the TensorBoard logs",
+    )
     arguments = parser.parse_args()
     return arguments
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_args()
 
     mnist = input_data.read_data_sets(args.dataset, one_hot=True)
@@ -51,14 +70,36 @@ if __name__ == '__main__':
     sequence_length = mnist.train.images.shape[1]
     model_choice = args.model
 
-    assert model_choice == '1' or model_choice == '2', "Invalid choice: Choose between 1 and 2 only."
+    assert (
+        model_choice == "1" or model_choice == "2"
+    ), "Invalid choice: Choose between 1 and 2 only."
 
-    if model_choice == '1':
-        model = CNN(alpha=1e-3, batch_size=128, num_classes=num_classes, num_features=sequence_length)
-        model.train(checkpoint_path=args.checkpoint_path, epochs=10000, log_path=args.log_path,
-                    train_data=mnist.train, test_data=mnist.test)
-    elif model_choice == '2':
-        model = CNNSVM(alpha=1e-3, batch_size=128, num_classes=num_classes, num_features=sequence_length,
-                       penalty_parameter=args.penalty_parameter)
-        model.train(checkpoint_path=args.checkpoint_path, epochs=10000, log_path=args.log_path,
-                    train_data=mnist.train, test_data=mnist.test)
+    if model_choice == "1":
+        model = CNN(
+            alpha=1e-3,
+            batch_size=128,
+            num_classes=num_classes,
+            num_features=sequence_length,
+        )
+        model.train(
+            checkpoint_path=args.checkpoint_path,
+            epochs=10000,
+            log_path=args.log_path,
+            train_data=mnist.train,
+            test_data=mnist.test,
+        )
+    elif model_choice == "2":
+        model = CNNSVM(
+            alpha=1e-3,
+            batch_size=128,
+            num_classes=num_classes,
+            num_features=sequence_length,
+            penalty_parameter=args.penalty_parameter,
+        )
+        model.train(
+            checkpoint_path=args.checkpoint_path,
+            epochs=10000,
+            log_path=args.log_path,
+            train_data=mnist.train,
+            test_data=mnist.test,
+        )
--- model/cnn_svm.py
+++ model/cnn_svm.py
@@ -17,8 +17,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-__version__ = '0.1.0'
-__author__ = 'Abien Fred Agarap'
+__version__ = "0.1.0"
+__author__ = "Abien Fred Agarap"
 
 import os
 import tensorflow as tf
@@ -27,7 +27,6 @@ import sys
 
 
 class CNNSVM:
-
     def __init__(self, alpha, batch_size, num_classes, num_features, penalty_parameter):
         """Initializes the CNN-SVM model
 
@@ -39,19 +38,23 @@ class CNNSVM:
         """
         self.alpha = alpha
         self.batch_size = batch_size
-        self.name = 'CNN-SVM'
+        self.name = "CNN-SVM"
         self.num_classes = num_classes
         self.num_features = num_features
         self.penalty_parameter = penalty_parameter
 
         def __graph__():
 
-            with tf.name_scope('input'):
+            with tf.name_scope("input"):
                 # [BATCH_SIZE, NUM_FEATURES]
-                x_input = tf.placeholder(dtype=tf.float32, shape=[None, num_features], name='x_input')
+                x_input = tf.placeholder(
+                    dtype=tf.float32, shape=[None, num_features], name="x_input"
+                )
 
                 # [BATCH_SIZE, NUM_CLASSES]
-                y_input = tf.placeholder(dtype=tf.float32, shape=[None, num_classes], name='actual_label')
+                y_input = tf.placeholder(
+                    dtype=tf.float32, shape=[None, num_classes], name="actual_label"
+                )
 
             # First convolutional layer
             first_conv_weight = self.weight_variable([5, 5, 1, 32])
@@ -59,14 +62,18 @@ class CNNSVM:
 
             input_image = tf.reshape(x_input, [-1, 28, 28, 1])
 
-            first_conv_activation = tf.nn.relu(self.conv2d(input_image, first_conv_weight) + first_conv_bias)
+            first_conv_activation = tf.nn.relu(
+                self.conv2d(input_image, first_conv_weight) + first_conv_bias
+            )
             first_conv_pool = self.max_pool_2x2(first_conv_activation)
 
             # Second convolutional layer
             second_conv_weight = self.weight_variable([5, 5, 32, 64])
             second_conv_bias = self.bias_variable([64])
 
-            second_conv_activation = tf.nn.relu(self.conv2d(first_conv_pool, second_conv_weight) + second_conv_bias)
+            second_conv_activation = tf.nn.relu(
+                self.conv2d(first_conv_pool, second_conv_weight) + second_conv_bias
+            )
             second_conv_pool = self.max_pool_2x2(second_conv_activation)
 
             # Fully-connected layer (Dense Layer)
@@ -74,8 +81,10 @@ class CNNSVM:
             dense_layer_bias = self.bias_variable([1024])
 
             second_conv_pool_flatten = tf.reshape(second_conv_pool, [-1, 7 * 7 * 64])
-            dense_layer_activation = tf.nn.relu(tf.matmul(second_conv_pool_flatten, dense_layer_weight) +
-                                                dense_layer_bias)
+            dense_layer_activation = tf.nn.relu(
+                tf.matmul(second_conv_pool_flatten, dense_layer_weight)
+                + dense_layer_bias
+            )
 
             # Dropout, to avoid over-fitting
             keep_prob = tf.placeholder(tf.float32)
@@ -87,22 +96,29 @@ class CNNSVM:
 
             output = tf.matmul(h_fc1_drop, readout_weight) + readout_bias
 
-            with tf.name_scope('svm'):
+            with tf.name_scope("svm"):
                 regularization_loss = tf.reduce_mean(tf.square(readout_weight))
                 hinge_loss = tf.reduce_mean(
-                    tf.square(tf.maximum(tf.zeros([batch_size, num_classes]), 1 - y_input * output)))
-                with tf.name_scope('loss'):
+                    tf.square(
+                        tf.maximum(
+                            tf.zeros([batch_size, num_classes]), 1 - y_input * output
+                        )
+                    )
+                )
+                with tf.name_scope("loss"):
                     loss = regularization_loss + penalty_parameter * hinge_loss
-            tf.summary.scalar('loss', loss)
+            tf.summary.scalar("loss", loss)
 
             optimizer = tf.train.AdamOptimizer(learning_rate=alpha).minimize(loss)
 
-            with tf.name_scope('accuracy'):
-                output = tf.identity(tf.sign(output), name='prediction')
-                correct_prediction = tf.equal(tf.argmax(output, 1), tf.argmax(y_input, 1))
-                with tf.name_scope('accuracy'):
+            with tf.name_scope("accuracy"):
+                output = tf.identity(tf.sign(output), name="prediction")
+                correct_prediction = tf.equal(
+                    tf.argmax(output, 1), tf.argmax(y_input, 1)
+                )
+                with tf.name_scope("accuracy"):
                     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-            tf.summary.scalar('accuracy', accuracy)
+            tf.summary.scalar("accuracy", accuracy)
 
             merged = tf.summary.merge_all()
 
@@ -115,9 +131,9 @@ class CNNSVM:
             self.accuracy = accuracy
             self.merged = merged
 
-        sys.stdout.write('\n<log> Building graph...')
+        sys.stdout.write("\n<log> Building graph...")
         __graph__()
-        sys.stdout.write('</log>\n')
+        sys.stdout.write("</log>\n")
 
     def train(self, checkpoint_path, epochs, log_path, train_data, test_data):
         """Trains the initialized model.
@@ -142,7 +158,9 @@ class CNNSVM:
 
         timestamp = str(time.asctime())
 
-        train_writer = tf.summary.FileWriter(logdir=log_path + timestamp + '-training', graph=tf.get_default_graph())
+        train_writer = tf.summary.FileWriter(
+            logdir=log_path + timestamp + "-training", graph=tf.get_default_graph()
+        )
 
         with tf.Session() as sess:
             sess.run(init)
@@ -150,7 +168,9 @@ class CNNSVM:
             checkpoint = tf.train.get_checkpoint_state(checkpoint_path)
 
             if checkpoint and checkpoint.model_checkpoint_path:
-                saver = tf.train.import_meta_graph(checkpoint.model_checkpoint_path + '.meta')
+                saver = tf.train.import_meta_graph(
+                    checkpoint.model_checkpoint_path + ".meta"
+                )
                 saver.restore(sess, tf.train.latest_checkpoint(checkpoint_path))
 
             for index in range(epochs):
@@ -159,34 +179,56 @@ class CNNSVM:
                 batch_labels[batch_labels == 0] = -1
 
                 # input dictionary with dropout of 50%
-                feed_dict = {self.x_input: batch_features, self.y_input: batch_labels, self.keep_prob: 0.5}
-                
+                feed_dict = {
+                    self.x_input: batch_features,
+                    self.y_input: batch_labels,
+                    self.keep_prob: 0.5,
+                }
+
                 # run the train op
-                summary, _, loss = sess.run([self.merged, self.optimizer, self.loss], feed_dict=feed_dict)
-                
+                summary, _, loss = sess.run(
+                    [self.merged, self.optimizer, self.loss], feed_dict=feed_dict
+                )
+
                 # every 100th step and at 0,
                 if index % 100 == 0:
-                    feed_dict = {self.x_input: batch_features, self.y_input: batch_labels, self.keep_prob: 1.0}
-                    
+                    feed_dict = {
+                        self.x_input: batch_features,
+                        self.y_input: batch_labels,
+                        self.keep_prob: 1.0,
+                    }
+
                     # get the accuracy of training
                     train_accuracy = sess.run(self.accuracy, feed_dict=feed_dict)
-                    
+
                     # display the training accuracy
-                    print('step: {}, training accuracy : {}, training loss : {}'.format(index, train_accuracy, loss))
+                    print(
+                        "step: {}, training accuracy : {}, training loss : {}".format(
+                            index, train_accuracy, loss
+                        )
+                    )
 
                     train_writer.add_summary(summary=summary, global_step=index)
 
-                    saver.save(sess, save_path=os.path.join(checkpoint_path, self.name), global_step=index)
+                    saver.save(
+                        sess,
+                        save_path=os.path.join(checkpoint_path, self.name),
+                        global_step=index,
+                    )
 
             test_features = test_data.images
             test_labels = test_data.labels
             test_labels[test_labels == 0] = -1
 
-            feed_dict = {self.x_input: test_features, self.y_input: test_labels, self.keep_prob: 1.0}
+            feed_dict = {
+                self.x_input: test_features,
+                self.y_input: test_labels,
+                self.keep_prob: 1.0,
+            }
 
             test_accuracy = sess.run(self.accuracy, feed_dict=feed_dict)
 
-            print('Test Accuracy: {}'.format(test_accuracy))
+            print("Test Accuracy: {}".format(test_accuracy))
 
     @staticmethod
     def weight_variable(shape):
@@ -216,7 +258,7 @@ class CNNSVM:
         :param weight: The size of the layer filter.
         :return: Returns a convolutional layer.
         """
-        return tf.nn.conv2d(features, weight, strides=[1, 1, 1, 1], padding='SAME')
+        return tf.nn.conv2d(features, weight, strides=[1, 1, 1, 1], padding="SAME")
 
     @staticmethod
     def max_pool_2x2(features):
@@ -225,4 +267,6 @@ class CNNSVM:
         :param features: The input to downsample.
         :return: Downsampled input.
         """
-        return tf.nn.max_pool(features, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+        return tf.nn.max_pool(
+            features, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME"
+        )

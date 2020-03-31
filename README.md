c205c8af0af2585bcedb14a93d444efd16707e44
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-03-31 19:06:30 +0800

    docs: update license year

--- README.md
+++ README.md
@@ -115,7 +115,7 @@ To cite the repository/software, kindly use the following BibTex entry:
 
 ## License
 ```
-Copyright 2017 Abien Fred Agarap
+Copyright 2017-2020 Abien Fred Agarap
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Abien Fred Agarap
+# Copyright 2017-2020 Abien Fred Agarap
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Implementation of the CNN classes"""
 from __future__ import absolute_import
 from __future__ import division

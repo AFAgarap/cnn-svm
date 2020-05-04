1f4611ef1cf476c066c680f4ad37f239547de09d
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-05-05 00:02:38 +0800

    docs: add license header

--- cnn_svm/utils/data.py
+++ cnn_svm/utils/data.py
@@ -1,3 +1,18 @@
+# Convolutional Neural Network with Support Vector Machine
+# Copyright (C) 2017-2020  Abien Fred Agarap
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """Utility functions module"""
 from __future__ import absolute_import
 from __future__ import division
